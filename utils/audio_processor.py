10f8cbfd5e91c556d24f9f43388522fff0776666
Suyash Jaiswal <work.suyashjaiswal@gmail.com>
2026-08-26 22:46:29 +0530

    Improve YouTube extraction compatibility

--- utils/audio_processor.py
+++ utils/audio_processor.py
@@ -9,15 +9,22 @@ def download_youtube_audio(url: str) -> str:
     output_path = os.path.join(DOWNLOAD_DIR, '%(title)s.%(ext)s')
 
     ydl_opts = {
-        'format': 'bestaudio/best',
-        'outtmpl': output_path,
-        'postprocessors': [{
-            'key': 'FFmpegExtractAudio',
-            'preferredcodec': 'wav',
-            'preferredquality': '192',
-        }],
-        'quiet': True,
-    }
+    "format": "bestaudio/best",
+    "outtmpl": output_path,
+    "noplaylist": True,
+    "quiet": True,
+    "no_warnings": True,
+    "extractor_args": {
+        "youtube": {
+            "player_client": ["android_vr"]
+        }
+    },
+    "postprocessors": [{
+        "key": "FFmpegExtractAudio",
+        "preferredcodec": "wav",
+        "preferredquality": "192",
+    }],
+}
 
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         info = ydl.extract_info(url, download=True)
